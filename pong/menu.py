70f0f7e9ffb9fb5b2d3939bda2a3ebc0dea9f4e9
Asianguy_123 <79142447+Asianguy123@users.noreply.github.com>
2021-02-22 02:39:28 +0000

    Add files via upload
    - created new home pong.py file to allow for importing menu
    - added a mode select page
    - added endless, first to 5, first to 11 modes for both splayer and mplayer
    - struggled with making it neat, had to use multiple extra files: plan to find a simpler method that doesnt require a new file to change a single variable

--- pong/menu.py
+++ pong/menu.py
@@ -1,4 +1,4 @@
-import pygame, sys, s_player, m_player
+import pygame, sys, controls, mode_s, mode_m
 
 def draw_text(text, font, color, surface, x, y):
     text_obj = font.render(text, 1, color)
@@ -6,12 +6,9 @@ def draw_text(text, font, color, surface, x, y):
     text_rect.center = (x, y)
     surface.blit(text_obj, text_rect)
 
-click = False
-
 #main menu
 def main_menu():
     while True:
-
         click = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,27 +17,33 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
-        
+
         draw_text('PONG', title_font, text_color, screen, screen_width/2, 50)
         draw_text('by Asianguy_123', name_font, text_color, screen, screen_width/2, 100)
 
         mx, my = pygame.mouse.get_pos()
 
-        button1 = pygame.Rect(screen_width/2 - 100, 200, 200, 50)
+        button1 = pygame.Rect(screen_width/2 - 100, 200, 200, 50)   
         button2 = pygame.Rect(screen_width/2 - 100, 300, 200, 50)
+        button3 = pygame.Rect(screen_width/2 - 100, 400, 200, 50)
         pygame.draw.rect(screen, accent_color, button1)
         draw_text('vs Computer', button_font, text_color, screen, screen_width/2, 225)
         pygame.draw.rect(screen, accent_color, button2)
         draw_text('2 Player', button_font, text_color, screen, screen_width/2, 325)
+        pygame.draw.rect(screen, accent_color, button3)
+        draw_text('Controls', button_font, text_color, screen, screen_width/2, 425)
+
         if button1.collidepoint(mx, my):
             if click:
-                s_player.run()
+                mode_s.score()
         if button2.collidepoint(mx, my):
             if click:
-                m_player.run()
-        
+                mode_m.score()
+        if button3.collidepoint(mx, my):
+            if click:
+                controls.main()
 
-        #rendering 
+        #rendering
         pygame.display.flip()
         clock.tick(120)
         screen.fill(bg_color)
@@ -52,7 +55,8 @@ pygame.display.set_caption('Pong | by Asianguy_123')
 screen_width = 1270
 screen_height = 648
 screen = pygame.display.set_mode((screen_width, screen_height))
-screen2 = pygame.display.set_mode((screen_width, screen_height))
+
+#colours + fonts
 bg_color = pygame.Color('#2F373F')
 accent_color = (27,35,43)
 text_color = (211, 79, 61)
@@ -60,4 +64,3 @@ title_font = pygame.font.Font('freesansbold.ttf', 80)
 name_font = pygame.font.Font('freesansbold.ttf', 24)
 button_font = pygame.font.Font('freesansbold.ttf', 24)
 
-main_menu()
